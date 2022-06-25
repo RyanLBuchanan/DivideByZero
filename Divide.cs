ee93ef4ed58e41b0379a6deea89144fd60a54793
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-06-25 17:19:04 -0600

    Add project files.

---
+++ Divide.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class Divide
+    {
+        static void Main(string[] args)
+        {
+            // Get the numerator
+            Console.Write("Please enter an integer numerator: ");
+            var numerator = int.Parse(Console.ReadLine());
+
+            // Get denominator
+            Console.Write("Please enter an integer denominator: ");
+            var denominator = int.Parse(Console.ReadLine());
+
+            // Divide the two integers, then display the result
+            var result = numerator / denominator;
+            Console.WriteLine(
+                $"\nResult: {numerator} / {denominator} = {result}");
+        }
+    }
+}
