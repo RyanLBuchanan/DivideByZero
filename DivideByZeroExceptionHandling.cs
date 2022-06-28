d99a7a51633e7c802e0c2ceb8c75f03222c530ae
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-06-28 17:41:17 -0600

    feat: Completed NegativeNumberException exercise

--- DivideByZeroExceptionHandling.cs
+++ DivideByZeroExceptionHandling.cs
@@ -1,49 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace DivideByZero
-{
-    class DivideByZeroExceptionHandling
-    {
-        static void Main(string[] args)
-        {
-            var continueLoop = true;  // Determines whether to keep looping
+//namespace DivideByZero
+//{
+//    class DivideByZeroExceptionHandling
+//    {
+//        static void Main(string[] args)
+//        {
+//            var continueLoop = true;  // Determines whether to keep looping
 
-            do
-            {
-                // Retrieve user input and calculate the quotient
-                try
-                {
-                    // int.Parse generates FormatException if argument cannot be converted to an integer
-                    Console.Write("Enter an integer numerator: ");
-                    var numerator = int.Parse(Console.ReadLine());
-                    Console.Write("Enter an integer denominator: ");
-                    var denominator = int.Parse(Console.ReadLine());
+//            do
+//            {
+//                // Retrieve user input and calculate the quotient
+//                try
+//                {
+//                    // int.Parse generates FormatException if argument cannot be converted to an integer
+//                    Console.Write("Enter an integer numerator: ");
+//                    var numerator = int.Parse(Console.ReadLine());
+//                    Console.Write("Enter an integer denominator: ");
+//                    var denominator = int.Parse(Console.ReadLine());
 
-                    // Division generates DivideByZeroException if denominator is 0
-                    var result = numerator / denominator;
+//                    // Division generates DivideByZeroException if denominator is 0
+//                    var result = numerator / denominator;
 
-                    // Display result
-                    Console.WriteLine(
-                        $"\nResult: {numerator} / {denominator} = {result}");
-                    continueLoop = false;
-                }
-                catch (FormatException formatException)
-                {
-                    Console.WriteLine($"\n{formatException.Message}");
-                    Console.WriteLine(
-                        $"You must enter two integers. Please try again.\n");
-                }
-                catch (DivideByZeroException divideByZeroException)
-                {
-                    Console.WriteLine($"\n{divideByZeroException.Message}");
-                    Console.WriteLine(
-                        "Zero is an invalid denominator. Please try again.\n");
-                }
-            } while (continueLoop);
-        }
-    }
-}
+//                    // Display result
+//                    Console.WriteLine(
+//                        $"\nResult: {numerator} / {denominator} = {result}");
+//                    continueLoop = false;
+//                }
+//                catch (FormatException formatException)
+//                {
+//                    Console.WriteLine($"\n{formatException.Message}");
+//                    Console.WriteLine(
+//                        $"You must enter two integers. Please try again.\n");
+//                }
+//                catch (DivideByZeroException divideByZeroException)
+//                {
+//                    Console.WriteLine($"\n{divideByZeroException.Message}");
+//                    Console.WriteLine(
+//                        "Zero is an invalid denominator. Please try again.\n");
+//                }
+//            } while (continueLoop);
+//        }
+//    }
+//}
---
+++ NegativeNumberException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideByZero
+{
+    public class NegativeNumberException : Exception
+    {
+        // Default constructor 
+        public NegativeNumberException()
+            : base ("Illegal operation for a negative number")
+        {
+            // Empty body
+        }
+
+        // Constructor for customizing error message 
+        public NegativeNumberException(string messageValue)
+            : base(messageValue)
+        {
+            // Empty body
+        }
+
+        // Constructor for customizing the exception's error message and specifying the InnerException object
+        public NegativeNumberException(string messageValue, Exception inner)
+            : base(messageValue, inner)
+        {
+            // Empty body
+        }
+    }
+}
