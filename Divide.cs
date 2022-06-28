e709f569b5193dc7eff32ffbfe49695faa5fb65e
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-06-28 07:48:27 -0600

    feat: Completed exception code

--- Divide.cs
+++ Divide.cs
@@ -1,23 +1,23 @@
-﻿using System;
+﻿//using System;
 
-namespace DivideByZero
-{
-    class Divide
-    {
-        static void Main(string[] args)
-        {
-            // Get the numerator
-            Console.Write("Please enter an integer numerator: ");
-            var numerator = int.Parse(Console.ReadLine());
+//namespace DivideByZero
+//{
+//    class Divide
+//    {
+//        static void Main(string[] args)
+//        {
+//            // Get the numerator
+//            Console.Write("Please enter an integer numerator: ");
+//            var numerator = int.Parse(Console.ReadLine());
 
-            // Get denominator
-            Console.Write("Please enter an integer denominator: ");
-            var denominator = int.Parse(Console.ReadLine());
+//            // Get denominator
+//            Console.Write("Please enter an integer denominator: ");
+//            var denominator = int.Parse(Console.ReadLine());
 
-            // Divide the two integers, then display the result
-            var result = numerator / denominator;
-            Console.WriteLine(
-                $"\nResult: {numerator} / {denominator} = {result}");
-        }
-    }
-}
+//            // Divide the two integers, then display the result
+//            var result = numerator / denominator;
+//            Console.WriteLine(
+//                $"\nResult: {numerator} / {denominator} = {result}");
+//        }
+//    }
+//}
--- DivideByZeroExceptionHandling.cs
+++ DivideByZeroExceptionHandling.cs
@@ -8,5 +8,42 @@ namespace DivideByZero
 {
     class DivideByZeroExceptionHandling
     {
+        static void Main(string[] args)
+        {
+            var continueLoop = true;  // Determines whether to keep looping
+
+            do
+            {
+                // Retrieve user input and calculate the quotient
+                try
+                {
+                    // int.Parse generates FormatException if argument cannot be converted to an integer
+                    Console.Write("Enter an integer numerator: ");
+                    var numerator = int.Parse(Console.ReadLine());
+                    Console.Write("Enter an integer denominator: ");
+                    var denominator = int.Parse(Console.ReadLine());
+
+                    // Division generates DivideByZeroException if denominator is 0
+                    var result = numerator / denominator;
+
+                    // Display result
+                    Console.WriteLine(
+                        $"\nResult: {numerator} / {denominator} = {result}");
+                    continueLoop = false;
+                }
+                catch (FormatException formatException)
+                {
+                    Console.WriteLine($"\n{formatException.Message}");
+                    Console.WriteLine(
+                        $"You must enter two integers. Please try again.\n");
+                }
+                catch (DivideByZeroException divideByZeroException)
+                {
+                    Console.WriteLine($"\n{divideByZeroException.Message}");
+                    Console.WriteLine(
+                        "Zero is an invalid denominator. Please try again.\n");
+                }
+            } while (continueLoop);
+        }
     }
 }
